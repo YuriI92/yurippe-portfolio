b97ca496b5bcf6021c3027984fc5062763eb6f8d
Yurippe <yurichikawa1992@gmail.com>
2022-07-09 22:59:00 -0400

    add ability to validate email and handle submit function

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
-function Contact(props) {
+function Contact() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errMsg, setErrMsg] = useState('');
+
+    const handleChange = event => {
+        // if the email field is changed,
+        if (event.target.name === 'email') {
+            const valid = validateEmail(event.target.value);
+
+            // if the valid is false, show err message
+            if (!valid) {
+                setErrMsg('Your email is invalid. Try again.');
+            }
+        // if the name or message field is changed,
+        } else {
+            // if the field's value is empty, show err message, or else set empty string
+            if (!event.target.value.length) {
+                setErrMsg('Please enter your name and message to submit.');
+            } else {
+                setErrMsg('');
+            }
+        }
+        
+        // if there was no error, set form's state to its value (and remain other fields as is)
+        if (!errMsg) {
+            setFormState({ ...formState, [event.target.name]: event.target.value });
+            console.log(formState);
+        }
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <section>
             <h2>Contact</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor='name'>Name:</label>
-                <input value={formState.name} type='text' name='name' />
+                <input 
+                    onBlur={handleChange}
+                    defaultValue={formState.name} 
+                    type='text' name='name' 
+                />
                 <label htmlFor='email'>Email address:</label>
-                <input value={formState.email} type='email' name='email' />
+                <input 
+                    onBlur={handleChange} 
+                    defaultValue={formState.email} 
+                    type='email' name='email' 
+                />
                 <label htmlFor='message'>Message:</label>
-                <textarea value={formState.message} name='message'></textarea>
+                <textarea 
+                    onBlur={handleChange} 
+                    defaultValue={formState.message} 
+                    name='message'
+                ></textarea>
+                {errMsg && (
+                    <p>{errMsg}</p>
+                )}
                 <button type='submit'>Submit</button>
             </form>
         </section>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,13 @@ export function capFirstLetter(str) {
     const firstLetter = str.charAt(0).toUpperCase();
     return firstLetter + str.substr(1);
 }
+
+export function validateEmail(email) {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    
+    if (email.match(regex)) {
+        return true;    
+    } else {
+        return false;
+    }
+}
