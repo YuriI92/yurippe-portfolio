ad2e86be3897aa9d5dc61eb1dea7cf80b1793390
Yurippe <yurichikawa1992@gmail.com>
2022-07-11 00:47:22 -0400

    add styles to resume

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -27,7 +27,6 @@ function Contact() {
         // if there was no error, set form's state to its value (and remain other fields as is)
         if (!errMsg) {
             setFormState({ ...formState, [event.target.name]: event.target.value });
-            console.log(formState);
         }
     }
 
@@ -37,31 +36,47 @@ function Contact() {
     }
 
     return (
-        <section>
-            <h2>Contact</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='name'>Name:</label>
-                <input 
-                    onBlur={handleChange}
-                    defaultValue={formState.name} 
-                    type='text' name='name' 
-                />
-                <label htmlFor='email'>Email address:</label>
-                <input 
-                    onBlur={handleChange} 
-                    defaultValue={formState.email} 
-                    type='email' name='email' 
-                />
-                <label htmlFor='message'>Message:</label>
-                <textarea 
-                    onBlur={handleChange} 
-                    defaultValue={formState.message} 
-                    name='message'
-                ></textarea>
-                {errMsg && (
-                    <p>{errMsg}</p>
-                )}
-                <button type='submit'>Submit</button>
+        <section className='row'>
+            <h2 className='col-12 d-sm-flex py-1'>
+                Contact
+            </h2>
+            <form onSubmit={handleSubmit} className='col-12 col-sm-7 col-lg-6 row mx-1'>
+                <div className='col-12'>
+                    <div className='col-12'>
+                        <label htmlFor='name'>Name:</label>
+                        <input 
+                            onBlur={handleChange}
+                            defaultValue={formState.name} 
+                            type='text' name='name'
+                            className='col-12' 
+                        />
+                    </div>
+                    <div className='col-12'>
+                        <label htmlFor='email'>Email address:</label>
+                        <input 
+                            onBlur={handleChange} 
+                            defaultValue={formState.email} 
+                            type='email' name='email' 
+                            className='col-12' 
+                        />
+                    </div>
+                    <div className='col-12'>
+                        <label htmlFor='message'>Message:</label>
+                        <textarea 
+                            onBlur={handleChange} 
+                            defaultValue={formState.message} 
+                            name='message'
+                            className='col-12' 
+                            rows='5'
+                        ></textarea>
+                    </div>
+                    {errMsg && (
+                        <p className='text-danger col-12'>{errMsg}</p>
+                    )}
+                    <button type='submit' className='m-3'>
+                        Submit
+                    </button>
+                </div>
             </form>
         </section>
     );
